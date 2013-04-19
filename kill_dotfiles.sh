d1b71ddb566fb64888eaa2df81663a447d1928a5
viszu <viszu84@gmail.com>
2013-04-19 04:03:31 +0200

    Add kill_dotfiles.sh to remove generated symlinks

---
+++ kill_dotfiles.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# remove symlinks
+# -h means that file is a symlink
+if [ -h ~/.profile ]; then
+  rm ~/.profile
+fi
+
+if [ -h ~/.bashrc ]; then
+  rm ~/.bashrc
+fi
+
+if [ -h ~/.bash_aliases ]; then
+  rm ~/.bash_aliases
+fi
+
+if [ -h ~/.gitconfig ]; then
+  rm ~/.gitconfig
+fi
+
+if [ -h ~/.gitignore_global ]; then
+  rm ~/.gitignore_global
+fi
+
+if [ -h ~/.vimrc ]; then
+  rm ~/.vimrc
+fi
+
+if [ -h ~/.tmux.conf ]; then
+  rm ~/.tmux.conf
+fi
+
+if [ -h ~/.ackrc ]; then
+  rm ~/.ackrc
+fi
+
+if [ -h ~/.config/bpython/config ]; then
+  rm ~/.config/bpython/config
+fi
+
+if [ -h ~/.config/redshift.conf ]; then
+  rm ~/.config/redshift.conf
+fi
+
+if [ -h ~/.local/share/radiotray/bookmarks.xml ]; then
+  rm ~/.local/share/radiotray/bookmarks.xml
+fi

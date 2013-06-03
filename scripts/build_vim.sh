e7fdbe9ef06974665fc17fdad14996698aa19830
viszu <viszu84@gmail.com>
2013-06-03 20:01:21 +0200

    Remove comment about rvm

--- scripts/build_vim.sh
+++ scripts/build_vim.sh
@@ -4,6 +4,5 @@
 # Dependencies (Install manually):
 # sudo apt-get build-dep vim-gnome
 # sudo apt-get install gnome-devel
-# rvm(Ruby Version Manager) is also needed for ruby-command
 
 hg clone https://vim.googlecode.com/hg/ /tmp/vim_source && cd /tmp/vim_source && ./configure --disable-nls --enable-multibyte --with-tlib=ncurses --enable-pythoninterp --with-python-config-dir=/usr/lib/python2.7/config --enable-rubyinterp --with-ruby-command=/home/viszu/.rbenv/versions/2.0.0-p195/bin/ruby --with-features=huge --enable-gui=gnome2 && make && sudo make install && rm -r /tmp/vim_source

8a7888b77afb11c068a5d8728590c2befff15a80
viszu <viszu84@gmail.com>
2013-06-05 20:50:41 +0200

    Rearrange

--- scripts/build_vim.sh
+++ scripts/build_vim.sh
@@ -5,4 +5,17 @@
 # sudo apt-get build-dep vim-gnome
 # sudo apt-get install gnome-devel
 
-hg clone https://vim.googlecode.com/hg/ /tmp/vim_source && cd /tmp/vim_source && ./configure --disable-nls --enable-multibyte --with-tlib=ncurses --enable-pythoninterp --with-python-config-dir=/usr/lib/python2.7/config --enable-rubyinterp --with-ruby-command=/home/viszu/.rbenv/versions/2.0.0-p195/bin/ruby --with-features=huge --enable-gui=gnome2 && make && sudo make install && rm -r /tmp/vim_source
+hg clone https://vim.googlecode.com/hg/ /tmp/vim_source && \
+cd /tmp/vim_source && \
+./configure --disable-nls \
+--with-features=huge \
+--enable-gui=gnome2 \
+--enable-multibyte \
+--with-tlib=ncurses \
+--enable-pythoninterp \
+--with-python-config-dir=/usr/lib/python2.7/config \
+--enable-rubyinterp \
+--with-ruby-command=`rbenv which ruby` && \
+make && \
+sudo make install && \
+rm -r /tmp/vim_source

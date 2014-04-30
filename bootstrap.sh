893c132f740bfe9820167eb5a1352f1d026651b5
viszu <viszu84@gmail.com>
2014-04-30 21:00:40 +0200

    Update

--- bootstrap.sh
+++ bootstrap.sh
@@ -80,8 +80,8 @@ symlink "viszu.zsh-theme" "$HOME/.oh-my-zsh/themes/viszu.zsh-theme"
 
 ###############################################################################
 
-mkdir -p ~/.vim/autoload
-curl -fLo ~/.vim/autoload/plug.vim https://raw.github.com/junegunn/vim-plug/master/plug.vim
+mkdir -p ~/.vim/bundle
+git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 
 if [ -e ~/.vimrc ]; then
   sudo vim +PlugUpdate +PlugClean +PlugUpgrade +qall
